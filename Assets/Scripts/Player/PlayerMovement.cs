762dc24e65963f7de9f83743c7d5e884058427ea
dubu-0 <yudin01@mail.ru>
2021-11-24 19:40:20 +0300

    Импортировал доп ассеты - motions и characters, добавил blend tree animation на игрока - меняется анимация (idle, walk, run) в завис. от скорости

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,15 +3,19 @@ using UnityEngine.AI;
 
 namespace Player
 {
+    [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(NavMeshAgent))]
     public class PlayerMovement : MonoBehaviour
     {
         private const string TerrainLayerName = "Terrain";
+        private static readonly int Speed = Animator.StringToHash("Speed");
         private NavMeshAgent _agent;
         private Camera _main;
+        private Animator _animator;
 
         private void Start()
         {
+            _animator = GetComponent<Animator>();
             _agent = GetComponent<NavMeshAgent>();
             _main = Camera.main;
         }
@@ -20,6 +24,8 @@ namespace Player
         {
             if (Input.GetMouseButtonDown(1)) 
                 Move();
+
+            BlendAnimation();
         }
 
         private void Move()
@@ -29,5 +35,7 @@ namespace Player
             if (Physics.Raycast(ray, out var hitInfo, float.MaxValue, LayerMask.GetMask(TerrainLayerName)))
                 _agent.SetDestination(hitInfo.point);
         }
+
+        private void BlendAnimation() => _animator.SetFloat(Speed, _agent.velocity.magnitude / _agent.speed);
     }
 }

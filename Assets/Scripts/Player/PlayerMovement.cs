dfe7608102add5075e183ee66e3a1e0ba171f03f
dubu-0 <yudin01@mail.ru>
2021-11-24 14:02:15 +0300

    Добавил игроку движение через navmesh

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Player
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private Transform targetToMove;
+
+        private NavMeshAgent _agent;
+
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            _agent.SetDestination(targetToMove.position);
+        }
+    }
+}
